f71313015608dea9484017c8c97a32a659c5db74
Jason Ly <jal192@pitt.edu>
2017-04-03 14:39:08 -0400

    Added javadoc comment, reorder import statements

--- src/com/laboon/RunButton.java
+++ src/com/laboon/RunButton.java
@@ -1,13 +1,16 @@
 package com.laboon;
 
 import java.awt.*;
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class RunButton extends JButton {
 
     private MainPanel _m;
     
+	/**
+	 * Add run button listener to the run button in main panel
+	 */
     public RunButton(MainPanel m) {
 	super("Run");
 	_m = m;
