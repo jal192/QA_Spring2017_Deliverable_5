edcf573b0f159cf4637158988433b2f635783c68
Jason Ly <jal192@pitt.edu>
2017-04-03 14:39:31 -0400

    Added javadoc comment, reorder import statements

--- src/com/laboon/StepButton.java
+++ src/com/laboon/StepButton.java
@@ -1,13 +1,16 @@
 package com.laboon;
 
 import java.awt.*;
-import javax.swing.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class StepButton extends JButton {
 
     private MainPanel _m;
     
+	/** 
+	 * Add step button listener for step button in main panel
+	 */
     public StepButton(MainPanel m) {
 	super("Step");
 	_m = m;
